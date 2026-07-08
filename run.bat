9dcb76c183fbe5b01f74349d7081610e7c84d610
Tonyjh07 <yjh07@qq.com>
2026-07-08 21:41:01 +0800

    run.bat

--- run.bat
+++ run.bat
@@ -1,13 +1,5 @@
 @echo off
 chcp 65001 >nul
 title Helium
-echo.
-echo  ====================================
-echo    Helium - 音频变速变调工具
-echo  ====================================
-echo.
-echo  Press Ctrl+C to stop the server
-echo.
 python "%~dp0app.py"
-echo.
 pause
